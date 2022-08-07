fd6f1e4f5c8c4958891b536c1203f6ffbd770d16
Brian Walters <brian.walters@hey.com>
2022-08-07 11:17:29 -0600

    Show image thumbnails on bird edit page, show images on FE

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
+    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
 ];
